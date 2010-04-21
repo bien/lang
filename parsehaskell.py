492e103ab0a1618ca923b60c2885f6d75d67a5c9
Michael Bieniosek <michael.bieniosek@gmail.com>
2010-04-20 20:20:39 -0700

    fix more problems

--- parsehaskell.py
+++ parsehaskell.py
@@ -1,13 +1,15 @@
 import traceback
 
-tokens = ('NAME', 'NUMBER', 'SCONST',
+reserved = { 'let': 'LET' }
+
+tokens = ['NAME', 'NUMBER', 'SCONST',
           'PLUS', 'MINUS', 'TIMES', 'DIVIDE', "MOD",
           'EQUALS', 'NOTEQUAL', 'LT', 'LTE', "GT", "GTE",
           'ASSIGN', 'ARROW',
           "AND", "OR", 
-          "LPAREN", "RPAREN", "LAMBDA")
+          "TYPE",
+          "LPAREN", "RPAREN", "LAMBDA"] + reserved.values()
 
-t_NAME = r"[a-zA-Z_][a-zA-Z0-9_']*"
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -26,7 +28,13 @@ t_LPAREN = r"\("
 t_RPAREN = r"\)"
 t_LAMBDA = r"\\"
 t_ASSIGN = r"="
+t_TYPE = r"::"
 t_SCONST = r'\"([^\\\n]|(\\.))*?\"'
+
+def t_NAME(t):
+    r"[a-zA-Z_][a-zA-Z0-9_']*"
+    t.type = reserved.get(t.value, 'NAME')
+    return t
 
 def t_NUMBER(t):
     r'\d+'
@@ -44,66 +52,97 @@ def t_error(t):
     raise Exception
 
 precedence = (
+    ('nonassoc', 'TYPE'),
+    ('right', 'ARROW'),
+    ('right', "OR"),
+    ('right', 'AND'),
+    ('nonassoc', 'EQUALS', 'NOTEQUAL', 'GT', "GTE", "LT", "LTE"),
     ('left', 'PLUS', "MINUS"),
     ('left', 'TIMES', 'DIVIDE', 'MOD'),
+    ('left', 'APPLY'),
 )
 
-start = 'stmt'
+start = 'decl'
 
-def p_infix_operator(t):
-    '''infix : PLUS
-    | MINUS
-    | TIMES
-    | DIVIDE
-    | MOD
-    | EQUALS 
-    | NOTEQUAL
-    | LT
-    | LTE
-    | GT
-    | GTE
-    | AND
-    | OR'''
-    t[0] = t[1]
 
 def p_expr_infix(t):
-   'expr : expr infix expr'
+   '''expr : expr PLUS expr
+   | expr MINUS expr
+   | expr TIMES expr
+   | expr DIVIDE expr
+   | expr MOD expr
+   | expr EQUALS expr
+   | expr NOTEQUAL expr
+   | expr LT expr
+   | expr LTE expr
+   | expr GT expr
+   | expr GTE expr
+   | expr AND expr
+   | expr OR expr'''
    t[0] = ((t[2], t[1]), t[3])
 
 def p_expr_atom(t):
-    '''expr : NUMBER
+    '''expr : atom'''
+    t[0] = t[1]
+
+def p_atom(t):
+    '''atom : NUMBER
     | NAME
     | SCONST'''
     t[0] = t[1]
 
 def p_expr_paren(t):
-    '''expr : LPAREN expr RPAREN'''
+    '''atom : LPAREN expr RPAREN'''
     t[0] = t[2]
 
 def p_expr_apply(t):
-    '''expr : expr expr'''
+    '''expr : expr expr %prec APPLY'''
     t[0] = ('apply', t[1], t[2])
 
 def p_params_single(t):
-    '''params : NAME'''
+    '''params : atom'''
     t[0] = [t[1]]
 
 def p_params_mult(t):
-    '''params : params NAME'''
-    t[0] = t[1]
+    '''params : atom params'''
+    t[0] = t[2]
     t[0].append(t[1])
 
 def p_expr_lambda(t):
-    '''expr : LAMBDA params ARROW expr'''
+    '''expr : LPAREN LAMBDA params ARROW expr RPAREN'''
     t[0] = ('lambda', t[2], t[4])
 
+def p_type_name(t):
+    '''type : NAME'''
+    t[0] = t[1]
+
+def p_type_paren(t):
+    '''type : LPAREN type RPAREN'''
+    t[0] = t[2]
+
+def p_type_func(t):
+    '''type : type ARROW type'''
+    t[0] = ('apply', t[1], t[3])
+
+def p_expr_type(t):
+    '''expr : expr TYPE type'''
+    t[0] = ('type', t[1], t[3])
+
 def p_assign(t):
-    '''stmt : NAME params ASSIGN expr'''
-    t[0] = ('bind', t[1], ('lambda', t[2], t[4]))
+    '''interactive : LET NAME params ASSIGN expr'''
+    t[0] = ('bind', t[2], ('lambda', t[3], t[5]))
 
 def p_eval_loop(t):
-    '''stmt : expr'''
+    '''interactive : expr'''
     t[0] = t[1]
+
+def p_decl_defun(t):
+    '''decl : NAME params ASSIGN expr'''
+    t[0] = ('bind', t[1], ('lambda', t[2], t[4]))
+
+def p_decl_type(t):
+    '''decl : NAME TYPE type'''
+    t[0] = ('type', t[1], t[3])
 
 def p_error(t):
     print "Syntax error at '%s'" % t.value
